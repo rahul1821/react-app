70582286988fbddb662c884b5ba11e789f06a8ef
Rahul Singh <rs388903@gmail.com>
2023-08-19 23:30:59 +0530

    init

---
+++ src/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navigation.css';
+
+export default function Navigation(){
+    return(
+        <div className='nav'>
+           <nav>
+            <ul className='navbar'>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Portfolio</li>
+
+            </ul>
+           </nav>
+        </div>
+    )
+};
